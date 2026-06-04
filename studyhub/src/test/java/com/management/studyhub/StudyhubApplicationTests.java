1a8c6280e0e1ba3ae3a6b257b45c88ba970b0f02
ducccc386 <ducccc386@gmail.com>
2026-06-04 20:11:42 +0700

    project structural

---
+++ studyhub/src/test/java/com/management/studyhub/StudyhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.studyhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

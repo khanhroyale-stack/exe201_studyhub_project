4cd1440d8e1e73538c63be0198456c26edff92aa
khanhroyale-stack <giakhanhlc2400@gmail.com>
2026-06-07 22:16:21 +0700

    s

--- studyhub-backend/src/main/java/com/management/studyhub/controller/TestimonialController.java
+++ studyhub-backend/src/main/java/com/management/studyhub/controller/TestimonialController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/testimonials")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin("*")
 @RequiredArgsConstructor
 public class TestimonialController {
 
